00ff23c2b63d03a5a0f0bacba0d7b5775c3f3cbb
bkh4149 <bkh4149@gmail.com>
2021-10-02 22:04:39 +0900

    感染者に当たったときぶよぶよするアニメを追加

--- seen2.py
+++ seen2.py
@@ -29,18 +29,22 @@ class Judge():
 
 class Player():
     def __init__(self, hp, level):
-        self.x = 400
+        self.x = 400            #位置
         self.y = 300
-        self.r1 = 15
-        self.vx = 0
+        self.r_org = 15         #オリジナルの半径
+        self.r = self.r_org     #半径
+        self.isAnime = False    #状態、アニメの切り替え
+        self.anime_count = 60   #アニメ用カウンター
+        self.vx = 0             #速度
         self.vy = 0
-        self.hitpoint = hp
+        self.hitpoint = hp      #
         self.red = 0
         self.blue = 250
         self.rate = self.blue/hp
         self.level = level
         self.isHit =False
-        self.tc = 60
+        self.tc = 60            #FPS
+
     def update(self):
         self.x += self.vx
         self.y += self.vy
@@ -67,18 +71,27 @@ class Player():
                 elif event.key==K_DOWN:
                     self.vy = temp 
                 elif event.key==K_SPACE:
-                    self.tc = 10 
+                    self.tc = 10 #スペースキーが押されたらFPSを10まで下げる
         if self.tc < 60:
             self.tc += 1
-        return int(self.tc)
+        return int(self.tc)#FPS値を返す、呼ぶ側で　ck.tick(tc)　として使っている
+
     def draw(self,screen):
-        if self.r1 > 15:#衝突時爆発
-            self.r1 -= 0.5
-            pygame.draw.circle(screen,(255,0,0),(int(self.x),int(self.y)),int(self.r1))
+        if self.isAnime :#衝突時爆発
+            self.r -= 1
+            if self.r < 3:
+                self.r = self.r_org #初期値に戻す
+            self.anime_count-=1
+            if self.anime_count<=0:
+                self.isAnime = False
+                self.anime_count = 120
+                self.r = self.r_org
+            pygame.draw.circle(screen,(255,0,0),(int(self.x),int(self.y)),int(self.r))
         else:
             self.blue = self.hitpoint*self.rate
             self.red = 255 - self.blue
-            pygame.draw.circle(screen,(int(self.red),0,int(self.blue)),(int(self.x),int(self.y)),int(self.r1))
+            pygame.draw.circle(screen,(int(self.red),0,int(self.blue)),(int(self.x),int(self.y)),int(self.r))
+
 class Ball():
     def __init__(self,x,y):
         self.x = x
@@ -90,6 +103,7 @@ class Ball():
         self.r = 5
         self.isHit = False
         self.hitAnime = 0
+
     def update(self,P):
         self.x += self.vx
         self.y += self.vy
@@ -98,15 +112,15 @@ class Ball():
         if self.y >= 600 or self.y < 0:
             self.vy *= -1
         #当たり判定
-        if abs(self.x-P.x) <= (P.r1+self.r):
-            if abs(self.y-P.y) <= (P.r1+self.r):
+        if abs(self.x-P.x) <= (P.r+self.r):
+            if abs(self.y-P.y) <= (P.r+self.r):
                 if self.isHit:
                     pass
                 else:
                     self.isHit = True
                     P.hitpoint -=1
                     P.isHit = True
-                    P.r1= 30
+                    P.isAnime = True
                     self.r = 15
                 #print(P.hitpoint)
         else:
@@ -120,13 +134,14 @@ class Ball():
             self.col = (10,10,10)
         pygame.draw.circle(screen,self.col,(int(self.x),int(self.y)),int(self.r))
 
+#--------------------------------------
 def seen2(screen,font,level,level_dict):
     isWin=False
     ck = pygame.time.Clock()
 
     #審判
-    sec=level_dict["sec"][level]
-    J1=Judge(sec)
+    sec = level_dict["sec"][level]
+    J1 = Judge(sec)
     #プレイヤ
     hp = level_dict["hp"][level]
     P1 = Player(hp, level)
@@ -143,7 +158,7 @@ def seen2(screen,font,level,level_dict):
             Bs[i].draw(screen)
         J1.update()
         J1.draw(screen, font, P1)
-        if P1.hitpoint < 2:
+        if P1.hitpoint <=0 :
             isWin = False
             break
         if J1.countDownTime < 0:
